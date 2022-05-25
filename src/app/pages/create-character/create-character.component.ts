79f49cffa8a8a1a73f8945ad00b83e48080159b3
Amanda-RVargas <amanda.jaques.vargas@outlook.com>
2022-05-24 23:07:27 -0300

    add all cards on create-character race

--- src/app/pages/create-character/create-character.component.ts
+++ src/app/pages/create-character/create-character.component.ts
@@ -1,16 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-create-character',
   templateUrl: './create-character.component.html',
   styleUrls: ['./create-character.component.css']
 })
+
 export class CreateCharacterComponent implements OnInit {
+  @Input() name: string;
+  @Input() checked: boolean;
 
   count:any;
 
-
-  constructor() { }
+  constructor() {   }
 
   ngOnInit(): void {
     this.count = 1;
@@ -20,5 +22,26 @@ export class CreateCharacterComponent implements OnInit {
     this.count ++;
   }
 
+  maxNo = false;
+  amt = 0;
+
+  onChange(isChecked: boolean) {
+    if (isChecked)
+      this.amt++
+    else 
+      this.amt--
+    this.amt === 2 ? this.maxNo = true : this.maxNo = false;
+  }
+
+  checkBox = [
+    { name: 'Força', checked: false },
+    { name: 'Destreza', checked: false },
+    { name: 'Constituição', checked: false },
+    { name: 'Inteligência', checked: false },
+    { name: 'Sabedoria', checked: false },
+    { name: 'Carisma', checked: false } 
+    ];
+
+    
 
 }
